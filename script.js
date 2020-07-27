0d3e8e1b24033b8a88081bc44ce3758847229d67
ConnectingGreen <68833413+ConnectingGreen@users.noreply.github.com>
2020-07-27 11:59:53 +0530

    added script but no functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+var mem1 = document.getElementById("1");
+
+function replaceContent1() {
+    mem1.innerHTML = "Hello"
+}
+
+mem1.addEventListener("click", replaceContent1);
+
+var slideShow = function(container) {
+    this.images = []a;
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+        this.images.push(container.children[i]);
+        this.images[i].style.display = "none";
+    }
+    
+    // Handle going to to the next slide
+    var nextSlide = function() {
+        for (var i = 0; i < this.images.length; i++) {
+            this.images[i].style.display = "none";
+        }
+        this.images[this.curImage].style.display = "block";
+        this.curImage++;
+        if (this.curImage >= this.images.length) {
+            this.curImage = 0;
+        }
+        window.setTimeout(nextSlide.bind(this), 1000);
+    };
+    
+    nextSlide.call(this);
+};
+slideShow(document.getElementById("slideshow"));
